4a79bac7682c9bca44fa43e6986255fdc819d995
teejbee <tjbalaji@gmail.com>
2017-10-31 19:28:11 +0530

    code modularized

--- motor.py
+++ motor.py
@@ -1,17 +1,19 @@
 import time 
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
 
 L1 = 19
 L2 = 26
 R1 = 6
 R2 = 13
 
-GPIO.setup(L1, GPIO.OUT)
-GPIO.setup(L2, GPIO.OUT)
-GPIO.setup(R1, GPIO.OUT)
-GPIO.setup(R2, GPIO.OUT)
+
+def init():
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setup(L1, GPIO.OUT)
+  GPIO.setup(L2, GPIO.OUT)
+  GPIO.setup(R1, GPIO.OUT)
+  GPIO.setup(R2, GPIO.OUT)
 
 def front_back(front):
     print "moving front {}\n".format(front)
@@ -21,18 +23,26 @@ def front_back(front):
     GPIO.output(R2, not front)
 
 
-ip = ' '
+def cleanup():
+    GPIO.cleanup()
 
-while ip != 'a':
+def loop():
+  ip = ' '
+  while ip != 'a':
     print 'enter '
     ip = raw_input()
     if ip == 'q':
-        front_back(True)
-        time.sleep(2)
+      front_back(True)
+      time.sleep(2)
     elif ip == 'z':
-        front_back(False)
-        time.sleep(2)
+      front_back(False)
+      time.sleep(2)
 
-
-GPIO.cleanup()
+def main():
+    init()
+    try:
+      loop()
+    except e:
+        print e.message
+    cleanup()
 
